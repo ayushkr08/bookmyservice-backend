c5eb6fcd5ec7583208bdd7f24ccd50694ba6a44e
Ayush Kumar <ayushishan08@gmail.com>
2026-02-03 11:05:57 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/bookmyservice/bookmyservice/BookMyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyservice.bookmyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmyservice/bookmyservice/BookMyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyservice.bookmyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
